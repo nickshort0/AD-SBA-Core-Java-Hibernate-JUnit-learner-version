730f2b4394c524e8508b82a6e1d880813850645d
nickshort0 <therealnickshort@gmail.com>
2022-05-16 16:31:43 -0700

    commit 2

--- src/main/java/sba/sms/models/Course.java
+++ src/main/java/sba/sms/models/Course.java
@@ -3,8 +3,8 @@ package sba.sms.models;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
 @Table(name = "course")
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,7 +13,6 @@ import java.util.List;
 @Setter
 @ToString
 @Entity
-
 public class Course {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,11 +29,24 @@ public class Course {
     @EqualsAndHashCode.Exclude
     @ToString.Exclude
     @ManyToMany(mappedBy = "courses", cascade = {CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}, fetch = FetchType.EAGER)
-    List<Student> students = new ArrayList<>();
+    //ArrayList<Student> students = new ArrayList<>();
+    Set<Student> students = new LinkedHashSet<>();
 
     public void addStudent(Student student){
         students.add(student);
         student.getCourses().add(this);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return id == course.id && name.equals(course.name) && instructor.equals(course.instructor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, instructor);
+    }
 }
--- src/main/java/sba/sms/models/Student.java
+++ src/main/java/sba/sms/models/Student.java
@@ -3,9 +3,7 @@ package sba.sms.models;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Table(name = "student")
 @NoArgsConstructor
@@ -31,22 +29,29 @@ public class Student {
 
     @EqualsAndHashCode.Exclude
     @ToString.Exclude
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH}, fetch = FetchType.EAGER)
     @JoinTable(name = "student_courses",
             joinColumns = @JoinColumn(name = "student_email"),
             inverseJoinColumns = @JoinColumn(name = "courses_id")
     )
-    List<Course> courses = new ArrayList<>();
+    //ArrayList<Course> courses = new ArrayList<>();
+    Set<Course> courses = new LinkedHashSet<>();
+
+    public void addCourse(Course course){
+        courses.add(course);
+        course.getStudents().add(this);
+    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Student student = (Student) o;
-        return email.equals(student.email) && name.equals(student.name) && password.equals(student.password) && Objects.equals(courses, student.courses);
+        return email.equals(student.email) && name.equals(student.name) && password.equals(student.password);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(email, name, password, courses);
+        return Objects.hash(email, name, password);
     }
 }
